6e778830ffd4367164698e59dbe389703d936216
Tobi Fanibi <58971180+tobifanibi@users.noreply.github.com>
2021-06-27 18:07:56 -0500

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-#npx create-react-app my-app
+npx create-react-app my-app
 sudo docker stop backend
 sudo docker rm backend
+sudo docker stop frontend
+sudo docker rm frontend
 sudo docker stop db
 sudo docker rm db
 sudo docker network create -d bridge my-net
