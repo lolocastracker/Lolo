39d87b1b80cf9ab25566820af7d4d5b8f145f7df
fanibi <tobifanibi@gmail.com>
2021-06-30 18:36:12 -0500

    changes

--- create.bat
+++ create.bat
@@ -5,6 +5,8 @@ docker stop frontend
 docker rm frontend
 docker stop db
 docker rm db
+docker stop nginx
+docker rm nginx
 docker network create -d bridge my-net
 docker-compose -f ./docker-compose.yml build
 docker-compose -f ./docker-compose.yml up
--- create.sh
+++ create.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-npx create-react-app my-app
-sudo docker stop backend
-sudo docker rm backend
-sudo docker stop frontend
-sudo docker rm frontend
-sudo docker stop db
-sudo docker rm db
+sudo docker stop map
+sudo docker rm map
+sudo docker stop comm
+sudo docker rm comm
+sudo docker stop user
+sudo docker rm user
+sudo docker stop react
+sudo docker rm react
+
+sudo docker stop mapdb
+sudo docker rm mapdb
+sudo docker stop commdb
+sudo docker rm commdb
+sudo docker stop userdb
+sudo docker rm userdb
+sudo docker stop nginx
+sudo docker rm nginx
+#npm install --prefix react 
+#npm install react-scripts@3.4.1 -g --prefix react 
+
 sudo docker network create -d bridge my-net
 sudo docker-compose -f ./docker-compose.yml build
 sudo docker-compose -f ./docker-compose.yml up
