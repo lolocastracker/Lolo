5acbeb04db5e9343f0fb47a50c880f0590fe6332
fanibi <tobifanibi@gmail.com>
2021-07-28 22:43:29 -0500

    just used the whole image

--- react/src/components/reportsubmit/Checkbox.js
+++ react/src/components/reportsubmit/Checkbox.js
@@ -1,32 +1,21 @@
 import React from "react";
-import people from '../../assets/report_submitted.png'; // Tell webpack this JS file uses this image
+import thankYouFull from '../../assets/thankYouFull.png'; // Tell webpack this JS file uses this image
 import { Grid,Segment,Image } from 'semantic-ui-react'
 
 
 
 export  const Checkbox = () => (
     <Grid padded>
-    <Grid.Row centered columns={3}  only='tablet' style={{marginTop:"400px"}}>
-      <Grid.Column style={{marginRight:"-1rem",paddingLeft:"0rem",paddingRight:"0rem"}}>
-      <Image src={people}/>
-      </Grid.Column>
-      <Grid.Column style={{marginLeft:"-1rem",paddingLeft:"0rem",paddingRight:"0rem"}}>
-      <Image src={people}/>
-      </Grid.Column>
-    </Grid.Row>
-
-    <Grid.Row centered columns={3}  only='computer' style={{marginTop:"400px"}}>
-      <Grid.Column style={{marginRight:"-1rem",paddingLeft:"0rem",paddingRight:"0rem"}}>
-      <Image src={people}/>
-      </Grid.Column>
-      <Grid.Column style={{marginLeft:"-1rem",paddingLeft:"0rem",paddingRight:"0rem"}}>
-      <Image src={people}/>
-      </Grid.Column>
-    </Grid.Row>
-
-    <Grid.Row centered columns={2} only='mobile' style={{marginTop:"200px"}}>
+    <Grid.Row centered columns={2}  only='tablet computer' style={{marginTop:"400px"}}>
       <Grid.Column>
-        <Image src={people}/>
+      <Image src={thankYouFull}/>
+      </Grid.Column>
+    </Grid.Row>
+
+
+    {/* <Grid.Row columns={2} centered only='mobile'>
+      <Grid.Column>
+      <Image src={people}/>
       </Grid.Column>
     </Grid.Row>
 
@@ -34,8 +23,9 @@ export  const Checkbox = () => (
       <Grid.Column>
       <Image src={people}/>
       </Grid.Column>
-    </Grid.Row>
+    </Grid.Row> */}
   </Grid>
+  
   
 )
 
