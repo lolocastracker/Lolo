9c45593cd25bee188de76a6e7463049ea4ab8c98
Travis Harris <travisvharris@gmail.com>
2021-07-29 21:15:24 -0700

    Open forum in new tab from navbar

--- react/src/components/auth/AuthButton.js
+++ react/src/components/auth/AuthButton.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useKeycloak } from '@react-keycloak/web';
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
-import ProfileButton from "./ProfileButton"
+// import ProfileButton from "./ProfileButton"
 
 
 const AuthButton = () => {
@@ -10,7 +10,7 @@ const AuthButton = () => {
    const { keycloak, initialized } = useKeycloak()
 
   return keycloak.authenticated ?<div><LogoutButton/> </div> :<LoginButton/>
-  return keycloak.authenticated ?<div><LogoutButton/> <ProfileButton/></div> :<LoginButton/>
+  // return keycloak.authenticated ?<div><LogoutButton/> <ProfileButton/></div> :<LoginButton/>
 };
 
 export default AuthButton;
